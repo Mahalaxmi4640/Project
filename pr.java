43f62dab43737bc932cdbf711119c3580f9b83f7
Mahalaxmi <mahalaxmi4640@gmail.com>
2021-11-24 09:44:09 +0530

    added pr

---
+++ pr.java
@@ -0,0 +1,23 @@
+class pr
+{
+	public static void main(String[] args)
+	 {
+	   System.out.println("Program starts....");
+
+	   //Declaration
+	   int a;
+	   int b;
+	   int c;
+
+	   //Initialization
+	   a = 10;
+	   b = 20;
+
+	   //Utilization
+	   c = a + b;
+
+	   System.out.println(c);
+	   
+	   System.out.println("Program ends....");	
+	}
+}
